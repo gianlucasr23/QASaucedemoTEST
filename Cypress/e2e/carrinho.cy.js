0b485a58e2190cc6609d8a75a4ebd93eac5f633c
Gian Lucas <lucas.gian90@gmail.com>
2026-03-19 17:35:52 -0300

    testes de login e carrinho realizados

---
+++ Cypress/e2e/carrinho.cy.js
@@ -0,0 +1,24 @@
+describe("Carrinho", () => {
+  it("Adicionar produto ao carrinho com sucesso", () => {
+    // Instruções do nosso teste (arrange)
+    cy.visit("https://www.saucedemo.com/");
+
+    cy.get('[data-test="username"]').type("standard_user");
+    cy.get('[data-test="password"]').type("secret_sauce");
+
+    cy.get('[data-test="login-button"]').click();
+
+    // Açoes que iremos válidar (act)
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+
+    // Assert
+    cy.get('[data-test="shopping-cart-badge"]')
+      .should("be.visible")
+      .and("have.text", "1");
+
+
+    cy.get('#shopping_cart_container').click()
+
+    cy.contains('Sauce Labs Backpack').should('be.visible')
+  });
+});
--- Cypress/e2e/login.cy.js
+++ Cypress/e2e/login.cy.js
@@ -1,11 +1,36 @@
-describe('Login', () => {
-    it ('Realizar login com sucesso' , () => {
-        // Arrange 
-        cy.visit('https://www.saucedemo.com/')
+describe("Login", () => {
+  it("Realizar login com sucesso", () => {
+    // Instruções do nosso teste (arrange)
+    cy.visit("https://www.saucedemo.com/");
 
-        // Act 
+    // Açoes que iremos válidar (act)
+    cy.get('[data-test="username"]').type("standard_user");
+    cy.get('[data-test="password"]').type("secret_sauce");
 
+    cy.get('[data-test="login-button"]').click();
 
-        // Assert
-    })
-})
+    // Assert
+
+    cy.url().should("equal", "https://www.saucedemo.com/inventory.html");
+  });
+
+  it("Realizar login informando credenciais inválidas", () => {
+    // Instruções do nosso teste (arrange)
+    cy.visit("https://www.saucedemo.com/");
+
+    // Açoes que iremos válidar (act)
+    cy.get('[data-test="username"]').type("user.invalid");
+    cy.get('[data-test="password"]').type("password.invalid");
+
+    cy.get('[data-test="login-button"]').click();
+
+    // Assert
+
+    cy.get('[data-test="error"]').should(
+      "contain.text",
+      "Username and password do not match any user in this service",
+    );
+
+    cy.url().should('equal' , 'https://www.saucedemo.com/')
+  });
+});
